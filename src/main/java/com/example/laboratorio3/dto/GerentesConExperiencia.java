8997531339b9ffb8bae6360c08db94c3dfd3f1ac
Katherine Obregon <a20181823@pucp.edu.pe>
2021-04-27 01:22:45 -0500

    ejercicio 4

---
+++ src/main/java/com/example/laboratorio3/dto/GerentesConExperiencia.java
@@ -0,0 +1,4 @@
+package com.example.laboratorio3.dto;
+
+public interface GerentesConExperiencia {
+}
--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -1,14 +1,23 @@
 package com.example.laboratorio3.repository;
 
 
+import com.example.laboratorio3.dto.EmpleadosSalarioMayor;
 import com.example.laboratorio3.entity.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
 
-//COMPLETAR
+    @Query(value = "select first_name , e.last_name, jh.start_date, jh.end_date, j.job_title , e.salary\n" +
+            "from employees e\n" +
+            "inner join job_history jh on e.employee_id = jh.employee_id\n" +
+            "inner join jobs j on e.job_id = j.job_id\n" +
+            "where e.salary>1500", nativeQuery = true)
+    List<EmpleadosSalarioMayor> obtenerEmpleadoSalario();
 
 }
