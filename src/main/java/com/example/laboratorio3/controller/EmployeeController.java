5313b574f21000662b7192d6ce5ef7209a0881a5
Jose Garcia - Acer <garcia.josea@pucp.edu.pe>
2021-04-27 00:32:22 -0500

    jose commit

--- src/main/java/com/example/laboratorio3/controller/EmployeeController.java
+++ src/main/java/com/example/laboratorio3/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,6 +29,12 @@ public class EmployeeController {
     @GetMapping("/listar")
     public String listaEmployee(Model model){
         model.addAttribute("listaEmployees", employeesRepository.findAll());
+        return "/employee/lista";
+    }
+
+    @PostMapping("/textSearch")
+    public String buscardor(@RequestParam("textBuscador") String textBuscador, Model model){
+        model.addAttribute("listaEmployees", employeesRepository.buscarInputBuscador(textBuscador));
         return "/employee/lista";
     }
 
--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -3,12 +3,22 @@ package com.example.laboratorio3.repository;
 
 import com.example.laboratorio3.entity.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
 
-//COMPLETAR
+    @Query(value = "select e.* from employees e\n" +
+            "left join jobs j on e.job_id = j.job_id\n" +
+            "left join departments d on e.department_id = d.department_id\n" +
+            "left join locations l on d.location_id = l.location_id\n" +
+            "where e.first_name like %?1% or e.last_name like %?1%\n" +
+            "or j.job_title like %?1% or d.department_name like %?1% or l.city like %?1%;",
+            nativeQuery = true)
+    List<Employees> buscarInputBuscador(String inputIngresado);
 
 }
